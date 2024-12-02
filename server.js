3ee78ec56a9dc55ab3bf6cc4664f94e4600e6a4d
yorkt1 <guilherme_oliveira18@estudante.sesisenai.org.br>
2024-12-01 23:39:21 -0300

    Iniciando com criptografia de senhas

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express();
+const bcrypt = require('bcrypt')
+
+app.use(express.json());
+
+
+const  users =[];
+
+app.get('/users', (req, res) => {
+    res.json(users)
+
+
+
+}) 
+
+app.post ('/users', async (req, res) =>{
+
+
+try{
+const  hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+}catch{
+    res.status(500).send();
+};
+
+
+})
+
+
+
+app.listen(3000)
